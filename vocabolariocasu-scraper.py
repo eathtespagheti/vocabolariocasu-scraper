7c2f8549be14d4b22b92b9338cd0de414d112e6b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-24 21:55:34 +0100

    WIP on definition parse

--- vocabolariocasu-scraper.py
+++ vocabolariocasu-scraper.py
@@ -3,10 +3,16 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def getLettersLinks(WEBPAGE_BASE: str):
+class Definition():
     """
-    Find all the letters links in the page
+    Single dictionary word with definition and examples
     """
+    word : str
+    definition: str
+    examples: list
+
+
+def getCenter(URL: str):
     # Make request
     page = requests.get(WEBPAGE_BASE)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -15,12 +21,43 @@ def getLettersLinks(WEBPAGE_BASE: str):
     body = list(html.children)[3]
     container = list(body.children)[1]
     content = list(container.children)[1]
-    center = list(content.children)[3]
+    return list(content.children)[3]
+
+
+def getLettersLinks(WEBPAGE_BASE: str):
+    """
+    Find all the letters links in the page
+    """
+    center = getCenter(WEBPAGE_BASE)
     ricerca = list(center.children)[1]
     links = {}
     for letter in ricerca.findAll('a'):
-        links[letter.text] = WEBPAGE_BASE + letter.get('href')
+        links[letter.text] = letter.get('href')
     return links
+
+
+def getWords(WEBPAGE_BASE: str, LetterLink: str):
+    """
+    Find all the definitions which starts with a certain letter
+    """
+    center = getCenter(WEBPAGE_BASE + LetterLink)
+    lemmi = list(center.children)[3]
+    links = {}
+    for word in lemmi.findAll('a'):
+        links[word.text] = letter.get('href')
+    return links
+
+
+def getDefinition(WEBPAGE_BASE: str, WordLink: str):
+    """
+    Find all the definitions which starts with a certain letter
+    """
+    center = getCenter(WEBPAGE_BASE + WordLink)
+    lemmi = list(center.children)[5]
+    testo = list(center.children)[3]
+    word = WordLink()
+    word.word = testo.find('strong').text
+    return word
 
 
 # Website base URL
@@ -28,3 +65,9 @@ WEBPAGE_BASE = "http://vocabolariocasu.isresardegna.it"
 
 lettersLinks = getLettersLinks(WEBPAGE_BASE)
 
+# For every letter
+for letter in lettersLinks:
+    words = getWords(WEBPAGE_BASE, lettersLinks[letter])
+    for word in words:
+        definition = getDefinition(WEBPAGE_BASE, words[word])
+        print(definition.word)
