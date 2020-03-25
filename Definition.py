f2b192d46e45ddc86284ab2464320ecd13a3617b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-25 21:42:01 +0100

    improve the definition fields

--- Definition.py
+++ Definition.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup, element
+
+
 class Definition():
     """
     Single dictionary word with definition and examples
@@ -5,4 +8,10 @@ class Definition():
     word: str
     definition: str
     examples: list
-    raw: str
+    raw: element.Tag
+
+    def parseWord(self):
+        self.word = self.raw.find('strong').text
+
+    def print(self):
+        print(self.word)
