3c55bdd5a9833044620b8fd846a94a1fb60443cf
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-26 09:49:31 +0100

    WIP on the percentage value

--- vocabolariocasu-scraper.py
+++ vocabolariocasu-scraper.py
@@ -1,33 +1,60 @@
 from getRequests import *
 from printingFunctions import *
+from fileIO import *
+import gc
 
 # Website base URL
 WEBPAGE_BASE = "http://vocabolariocasu.isresardegna.it"
+OUT_FOLDER = "output"
 
+jsonWordsReference = []
+numberOfDefinitions = 0
+processedDefinitions = 0
+
+# Get all the links and words
+print("Downloading all the words' definitions' links")
 lettersLinks = getLettersLinks(WEBPAGE_BASE)
-dictionary = []
-
-# For every letter
 numberOfLetters = len(lettersLinks)
 for i, letter in enumerate(lettersLinks, start=1):
-    # Verbose
-    printPercentage(i, numberOfLetters)
-    print("Working on letter " + letter)
+    printProgress(i, numberOfLetters)
+    print("Downloading links for letter " + letter, )
     # Parse all the words from page
     words = getWords(WEBPAGE_BASE, lettersLinks[letter])
-    # Array of definitions
-    definitions = []
-    # Dict with the definitions associated to the letter
-    wordsDict = {str(letter): definitions}
-    dictionary.append(definitions)
-    # Number of words
-    numberOfWords = len(words)
-    for j, word in enumerate(words, start=1):
-        # Verbose
-        printTab(1)
-        printPercentage(j, numberOfWords)
-        print("Working on word " + word)
-        # Get definition
-        definition = getDefinition(WEBPAGE_BASE, words[word])
-        definitions.append(definition)
-        definition.print()
+    # Save words to a json file
+    jsonpath = saveDictToJSON(words, "__words", path.join(OUT_FOLDER, letter))
+    # Add jsonpath to list
+    jsonWordsReference.append(jsonpath)
+    # Update number of definitions
+    numberOfDefinitions += len(words)
+    # Free memory
+    del words
+    gc.collect()
+# Free memory again
+del lettersLinks
+gc.collect()
+
+
+# # For every letter
+# numberOfLetters = len(lettersLinks)
+# for i, letter in enumerate(lettersLinks, start=1):
+#     # Verbose
+#     printProgress(i, numberOfLetters)
+#     print("Working on letter " + letter)
+#     # Parse all the words from page
+#     words = getWords(WEBPAGE_BASE, lettersLinks[letter])
+#     # Array of definitions
+#     definitions = []
+#     # Dict with the definitions associated to the letter
+#     wordsDict = {str(letter): definitions}
+#     dictionary.append(definitions)
+#     # Number of words
+#     numberOfWords = len(words)
+#     for j, word in enumerate(words, start=1):
+#         # Verbose
+#         printTab(1)
+#         printProgress(j, numberOfWords)
+#         print("Working on word " + word)
+#         # Get definition
+#         definition = getDefinition(WEBPAGE_BASE, words[word])
+#         definitions.append(definition)
+#         definition.print()
